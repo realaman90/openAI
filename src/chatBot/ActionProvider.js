44d96efcb9a45ec1b58becc189f6a5c3ab32431d
utkarsh-dev <utkarsh17ife@gmail.com>
2023-02-22 16:30:20 +0530

    save context

--- src/chatBot/ActionProvider.js
+++ src/chatBot/ActionProvider.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-    const handleGreeting = async (inputMessage) => {
+  const handleGreeting = async (inputMessage) => {
+    const url = `/api/openAI?message=${encodeURIComponent(inputMessage)}`;
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const url = `/api/openAI?message=${encodeURIComponent(inputMessage)}`;
-        const response = await fetch(url);
-        const data = await response.json();
+    const outputMessage = createChatBotMessage(data.message.text);
 
-        const outputMessage = createChatBotMessage(data.message.text);
-        setState((prevState) => ({
-            ...prevState,
-            messages: [...prevState.messages, outputMessage],
-        }));
-    }
-    return (
-        <div>
-            { React.Children.map(children, (child) => {
-                return React.cloneElement(child, {
-                    actions: {
-                        handleGreeting,
-                    },
-                });
-            }) }
-        </div>
-    );
+    setState((prevState) => ({
+      ...prevState,
+      messages: [...prevState.messages, outputMessage],
+    }));
+  };
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+            handleGreeting,
+          },
+        });
+      })}
+    </div>
+  );
 };
 
 export default ActionProvider;
--- src/pages/api/openAI.js
+++ src/pages/api/openAI.js
@@ -2,27 +2,17 @@ import openai from 'openai';
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
+const chats = [];
+
 export default async function handler(req, res) {
-  const previousChat = [
-    'User: Hi, can you help me with my order?',
-    'Bot: Sure, what do you need help with?',
-    "User: I can't seem to find the checkout button.",
-    'Bot: It should be at the bottom of the page. Can you check again?',
-    "Bot:If you still can't find it, let me know and I can help you complete your order.",
-    "User: yes, please I want to finsh buying the cart",
-    // Add more chat messages as needed
-  ];
-  const context = previousChat.join('\n');
-  
-  const prompt = `Context:
-
-${context}
-
-New message:
-
-`;
-
   const { Configuration, OpenAIApi } = require('openai');
+
+  chats.push(`User: ${req.query.message}`);
+
+  let prompt = `Context:
+  ${chats.join('\n')}
+  New Message:
+  `;
 
   const configuration = new Configuration({
     apiKey: OPENAI_API_KEY,
@@ -31,7 +21,7 @@ New message:
   try {
     const completion = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: prompt,
+      prompt,
       max_tokens: 1000,
 
       temperature: 0,
@@ -39,29 +29,13 @@ New message:
       frequency_penalty: 0.2,
       presence_penalty: 0.0,
     });
-    console.log(completion);
+
     const response = completion.data.choices[0];
-    console.log(response);
+
+    chats.push(`New Message: ${response.text.replace(/\n/g, '')}`);
+
     res.status(200).json({ message: response });
   } catch (error) {
     console.log(error);
   }
 }
-
-// const { Configuration, OpenAIApi } = require("openai");
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-
-// const response = await openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-//   temperature: 0,
-//   max_tokens: 100,
-//   top_p: 1,
-//   frequency_penalty: 0.0,
-//   presence_penalty: 0.0,
-//   stop: ["\n"],
-// });
